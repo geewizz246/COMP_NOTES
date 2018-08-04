0796c972d53eaaed7a7ae08ae6ce2a8f6d113136
geewizz246 <geewizz246@users.noreply.github.com>
2018-08-04 11:34:14 -0400

    ._.

--- C++/Derek_Banas_Tutorial/Tutorial-14/Tutorial14_part4.cpp
+++ C++/Derek_Banas_Tutorial/Tutorial-14/Tutorial14_part4.cpp
@@ -7,24 +7,23 @@
 #include <deque> // Double Ended Queue
 
 int main() {
-
     // ----- CONTAINERS -----
     // We have already seen the STL container vector
     // There are many other special ways of storing data
 
     // ----- DOUBLE ENDED QUEUE -----
 
-    // A double ended queue (Deck) is a dynamic array that can 
-    // be expanded or contracted on both ends
+    // A double ended queue (commmonly called 'Deck') is a dynamic array that can be expanded 
+    // or contracted on both ends
     std::deque<int> nums = { 1,2,3,4 };
     nums.push_front(0);
     nums.push_back(5);
     for (int x : nums)
         std::cout << x << "\n";
 
-    // You can access index values, but they are costly
-    // because values aren't stored contigously, but
-    // instead use multiple arrays
+    // You can access index values, but they are costly because values aren't stored contiguously,
+    // but instead use multiple arrays
+    // NB : contiguous - near or next to but not actually touching
     std::cout << nums[0] << "\n";
 
     // ----- END DOUBLE ENDED QUEUE -----
@@ -58,6 +57,7 @@ int main() {
     // Previous moves a set number of indexes and returns an iterator
     auto itr4 = prev(itr2, 1);
     std::cout << "Previous : " << *itr4 << "\n";
+
     // You can also insert at a defined index
     std::vector<int> nums3 = { 1, 4, 5, 6 };
     std::vector<int> nums4 = { 2, 3 };
