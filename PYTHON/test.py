871b84a17606d051c10726c1941504b9778d2058
geewizz246 <geewizz246@users.noreply.github.com>
2018-08-20 16:26:28 -0400

    Removed Python code

--- PYTHON/test.py
+++
@@ -1,7 +0,0 @@
-import time  # imports time library functions to the program
-import random  # imports random library functions to the program
-
-print("Sleeping...")
-time.sleep(1)  # makes the program sleep for 1 second
-print("Awake")
-print(random.randint(0, 10))  # prints a random number between 0 and 10 inclusive
